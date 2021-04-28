e8de04de18e6eacab2d7fa3f3aa96aa9ed21bb00
Thiago Rocha Moreira Paiva <pste13.informatica@crecisp.gov.br>
2021-04-28 18:50:38 -0300

    Pra funcionar

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tiledesk External Chatbot Hello world tutorial 
+
+Please follow [this document](https://developer.tiledesk.com/external-chatbot/connect-your-own-chatbot) on Tiledesk [developer zone](https://developer.tiledesk.com/) for detailed instructions on how to setup this tutorial.
---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const { TiledeskChatbotClient } = require('@tiledesk/tiledesk-chatbot-client')
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.post('/thechatbot/v1/answerme', (req, res) => {
+  const tdclient =  new TiledeskChatbotClient({request: req, response: res})
+  console.log('You asked: ' + tdclient.text)
+  res.status(200).send({"success":true})
+  let msg = {
+    text: 'Cheers! You asked: ' + tdclient.text
+  }
+  tdclient.sendMessage(msg)
+});
+
+
+app.listen(29005, () => {
+  console.log('server started')
+})
