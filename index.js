40d3935e92039ebcd645e47571abbc62d1bb7fed
Thiago Rocha Moreira Paiva <pste13.informatica@crecisp.gov.br>
2021-04-30 16:31:56 -0300

    Bronca se rolar nome feio!

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const functions = require('./lib/functions.js')
+
 const { TiledeskChatbotClient } = require('@tiledesk/tiledesk-chatbot-client')
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.post('/thechatbot/v1/answerme/tiledesk', (req, res) => {
-  const tdclient =  new TiledeskChatbotClient({request: req, response: res})
+app.post('/thechatbot/v1/answerme/:prm', (req, res) => {  
+  const reqCameFromTiledesk = ('tiledesk'==req.params.prm)
+  const tdclient = (reqCameFromTiledesk ?  new TiledeskChatbotClient({request: req, response: res}) : {text:req.body.pergunta})
   console.log('You asked: ' + tdclient.text)
-  res.status(200).send({"success":true})
+  if(reqCameFromTiledesk){
+    res.status(200).send({"success":true})
+  }
   let msg = {
     text: functions.processQuestion(tdclient.text,false)
   }
-  tdclient.sendMessage(msg)
+  
+  if(reqCameFromTiledesk){
+    tdclient.sendMessage(msg)
+  }else{
+    res.status(200).send({"resposta":msg.text})
+  }
 });
 
 
